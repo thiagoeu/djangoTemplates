83d841ca4a610cbe73c9837b18907166576b6db0
thiagoeu <araujo.thiago1051@gmail.com>
2024-12-15 00:22:19 -0300

    feat: posts adicionado

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
     path('about/', views.about),
+    path('posts/', include('posts.urls'))
     
 ]
