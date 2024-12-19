870b5b2543882d406a0a3995f6fffbdc08b1c1df
thiagoeu <araujo.thiago1051@gmail.com>
2024-12-19 10:52:37 -0300

    feat: adicionando configurações para imagens

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -111,7 +111,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
